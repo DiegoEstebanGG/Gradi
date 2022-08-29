2739671d2827e3b8ba973916f377a296c6bde1ee
Diego Gutierrez <dgutierrez.gutierrez176@gmail.com>
2022-08-29 10:59:29 -0500

    codigo estilos

--- request.js
+++ request.js
@@ -9,8 +9,27 @@ fetch(url)
 const mostrarDatos = (data) => {
     let products = JSON.parse(data);
     console.log(products);
+    //descripcion
+    imgt = document.getElementById("img").src = +products["media"][0]['preview_image']['src'];
+    console.log(imgt)
+        //img = '<img>' 
+        //document.getElementById('img').innerHTML = img
+        //titulo
     titulo = '<h1>' + products.title;
     document.getElementById('titulo').innerHTML = titulo
+        //precio
+    precio = '<h3>' + products.price;
+    document.getElementById('precio').innerHTML = precio
+        //precio sin descuento
+    preciod = '<h5>' + products.compare_at_price;
+    document.getElementById('preciod').innerHTML = preciod
+        //color
+    opc = '<h5>' + products['options'][0]['values'][0];
+    document.getElementById('opc').innerHTML = opc
+        //talla
+    opc = '<h5>' + products['options'][0]['values'][1];
+    document.getElementById('opc2').innerHTML = opc
+        //descripcion
     content = '<p>' + products.description;
     document.getElementById('descrip').innerHTML = content
 }
