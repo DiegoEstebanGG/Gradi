51a4da927955630e3132d95a28add517f2a0712a
Diego Gutierrez <dgutierrez.gutierrez176@gmail.com>
2022-08-28 22:03:53 -0500

    codigo insertando datos de shopify

--- request.js
+++ request.js
@@ -1,2 +1,16 @@
-const request = new XMLHttpRequest();
-request.open('GET', 'https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js', true);
+let url = 'https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js'
+
+fetch(url)
+    .then(response => response.text())
+    .then(data => mostrarDatos(data))
+    .catch(error => console.log(error))
+
+
+const mostrarDatos = (data) => {
+    let products = JSON.parse(data);
+    console.log(products);
+    titulo = '<h1>' + products.title;
+    document.getElementById('titulo').innerHTML = titulo
+    content = '<p>' + products.description;
+    document.getElementById('descrip').innerHTML = content
+}
